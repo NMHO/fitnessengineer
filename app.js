911b6d25db11487fc86409662660ac424fe3a353
Victor Nielsen <victor.nielsen@hotmail.com>
2016-11-14 11:51:00 +0100

    deleted test

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-//Test
-
-
 require('./models/db');
 
 var routes = require('./routes/index');
