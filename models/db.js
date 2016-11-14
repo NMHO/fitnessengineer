5726bbdcef45633b94addec67f9e7fa5dca04077
NMHO <Nicklas-MacBook@Nicklass-MacBook-Pro.local>
2016-11-14 12:13:10 +0100

    db uri

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require( 'mongoose' );
 var gracefulShutdown;
 //var dbURI = 'mongodb://localhost/FitnessDB';
-var dbURI = 'mongodb://<fitnessapp>:<fitnessapp2016>@ds147487.mlab.com:47487/heroku_q3q7179z'
+var dbURI = 'mongodb://fitnessapp:fitnessapp2016@ds147487.mlab.com:47487/heroku_q3q7179z'
 mongoose.connect(dbURI);
 
 //Test
